6b1131b3b4a99d1ab0532b670d3c381b8be8d3c0
bw.y <i@bw-y.com>
2016-08-14 16:08:35 +0800

    next to chapter-2

--- run.sh
+++ run.sh
@@ -5,7 +5,8 @@ export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 src=$1.cpp
 exe_bin=$1
+shift
 echo "# start"
-g++ -o $exe_bin $src && ./$exe_bin;echo " $?"
+g++ -o $exe_bin $src && ./$exe_bin $@;echo " $?"
 rm -f $exe_bin
 echo "# end"
